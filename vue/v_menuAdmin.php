8c5f990eb2d85960adbd14c53c9ef5f273eb7c8c
Massiré <73407799+Massire9@users.noreply.github.com>
2021-01-18 15:31:02 +0100

    creation gestion admin
    v_gestionAdmin a été crée dans le dossier vue

--- vue/v_menuAdmin.php
+++ vue/v_menuAdmin.php
@@ -16,33 +16,6 @@
 					<a href="">ajouter</a>							
 				</li>
 			</ul>		
-		</li>
-		<li>
-			<a href="">       </a>
-		</li>
-		<li>
-		<a href="">           </a>
-	
-		</li>
-		<li>
-		<a href="">             </a>
-	
-		</li>
-		<li> 
-			<a href="">membres </a>
-			<ul>
-				<li>
-					<a href="index.php?uc=menuAdmin&action=ajouterMembre">Ajouter</a>					
-				</li>		
-				<li>
-					<a href="index.php?uc=menuAdmin&action=listeMembre">Modifier</a>
-				</li>
-					<li>
-					<a href="index.php?uc=menuAdmin&action=supprimerMembre">Supprimer</a>
-				</li>
-			</ul>		
-		</li>
-		
 		<li><a href="index.php?uc=connexion&action=deconnexion">deconnexion</a></li>
 	</ul>
 </nav>
